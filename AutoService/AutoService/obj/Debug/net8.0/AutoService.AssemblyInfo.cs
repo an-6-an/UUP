cde131cf37cf149fd9939e05e7bcda8faedabc22
an-6-an <afarlenkova890@gmail.com>
2026-03-03 21:46:34 +0500

    подправила отчёт

--- AutoService/AutoService/obj/Debug/net8.0/AutoService.AssemblyInfo.cs
+++ AutoService/AutoService/obj/Debug/net8.0/AutoService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af421dc0c3eb7a632bb9355f54aea4e4f298a011")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c2fb27d2f330cb0c67e15d966189a78036363a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
